8050a2b3a1bc7a1b2465c774fa6f407e3b13e5ab
meghamoree <meghamoree@gmail.com>
2020-05-05 19:19:57 +0530

    Add.Adding new feature to check FirstName.

--- Userregistration.sh
+++ Userregistration.sh
@@ -1,2 +1,16 @@
 #!/bin/bash -x
+echo "Welcome to User Registration"
+function FirstName()
+	{
+		read -p "Enter the first name : " firstName
+		pattern="^[A-Z][a-z]{3,}$"
+		if [[ $firstName =~ $pattern ]]
+		then
+			echo "okay"
+		else
+			echo "not okay"
 
+
+		fi
+	}
+FirstName
